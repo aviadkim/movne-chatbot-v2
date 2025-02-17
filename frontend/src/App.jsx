fa9d71e4ff2e35e2cabf51bbff641f351cf1ba44
aviadkim <aviad@kimfo-fs.com>
2025-02-17 20:59:45 +0000

    Initial project setup with all required files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatInterface from './components/Chat/ChatInterface';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default App;
